d0a32f911922fa4d98c43571a4aeae617641cc39
bersiroth <caronbernard1@gmail.com>
2022-11-13 17:30:40 +0100

    refactor(architecture): clean for ci

--- tests/fixture.py
+++ tests/fixture.py
@@ -3,11 +3,11 @@ from unittest.mock import patch
 
 import pytest
 
-Terminal = namedtuple("terminal", ["lines", "columns"])
+Terminal = namedtuple("Terminal", ["lines", "columns"])
 
 
 @pytest.fixture(scope="session", autouse=True)
 def os_terminal() -> None:
     terminal = Terminal(24, 80)
-    os = patch('highway_code.domain.question.render.os').start()
+    os = patch("highway_code.domain.question.render.os").start()
     os.get_terminal_size.return_value = terminal
--- tests/unit/application/question/test_handler.py
+++ tests/unit/application/question/test_handler.py
@@ -1,20 +1,18 @@
-from unittest.mock import seal
-
-from pytest_mock import MockerFixture
+from unittest.mock import seal, Mock
 
 from highway_code.application.question.handler import QuestionCommand, QuestionHandler
 from tests.unit.factory import question_factory_test
 
 
-def test_question_handler(mocker: MockerFixture) -> None:
-    question_manager_mock = mocker.patch("highway_code.domain.question.manager.QuestionManager")
+def test_question_handler() -> None:
     question = question_factory_test()
+    question_manager_mock = Mock()
     question_manager_mock.select_question.return_value = question
     question_manager_mock.process_question.return_value = True
     question_manager_mock.question_list_is_empty.return_value = False
     question_manager_mock.ask_question_again.return_value = False
     seal(question_manager_mock)
-    statistic_manager_mock = mocker.patch("highway_code.domain.statistic.manager.StatisticManager")
+    statistic_manager_mock = Mock()
     statistic_manager_mock.add_answer()
     statistic_manager_mock.show_statistic()
     statistic_manager_mock.save()
